a463003bfe2669e700b5fe5a009d743ad75619f5
Metodi Gachev <metodi.gachev@scalefocus.com>
2022-07-13 16:41:33 +0300

    Add Index controller, make searching by description for Category and UnitOfMeasure.

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,32 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"/", "", "/index"})
+    public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasure.get());
+
+        return "index";
+    }
+}
