9fcf8f0c095613f59e74e273f55bd0a2efc12211
Metodi Gachev <metodi.gachev@scalefocus.com>
2022-07-13 16:22:25 +0300

    Add some fields in the database.

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -2,7 +2,6 @@ package guru.springframework.repositories;
 
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 }
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -2,7 +2,6 @@ package guru.springframework.repositories;
 
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 }
